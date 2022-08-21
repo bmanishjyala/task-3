3900b6a4a33c46029c1ca894990964ed74e9c7eb
codefox <bmanishjyala@gmail.com>
2022-08-21 07:23:52 -0700

    task 3 completed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+// showing preview
+
 $("#todo").keyup(function () {
     $("#typing").text($(this).val());
     if ($(this).val()) {
@@ -5,3 +9,58 @@ $("#todo").keyup(function () {
 
     }
 });
+
+// add todo
+
+$('#add_todo_btn').bind("click", function () {
+    if ($('#todo').val() != "") {
+        $('tbody').append(` <tr>
+    
+        
+        <td >
+        <input type="checkbox" class="form-check-input"  onclick="todoStatus($(this))" >
+    
+        </td>
+        <td >
+        <p class="fw-bold px-2 " style="letter-spacing:2px"  >${$('#todo').val()}</p>
+    
+        </td>
+    
+        <td>
+            <button type="button" onclick="$(this).parent().parent().remove()" class="btn btn-link btn-sm btn-rounded">
+                Del
+            </button>
+        </td>
+    </tr>`)
+    $('#todo').val('');
+    $('#typing').text('');
+
+    } else {
+        $('#modal_button').click();
+    }
+
+})
+
+// delete selected 
+const deleteSelected=()=>{
+    $('input[type=checkbox]').each(function () {
+        if(this.checked){
+            $(this).parent().parent().remove();
+        }
+    })
+
+}
+
+
+const todoStatus=(e)=>{
+
+    if(e.is(":checked")){
+        $(e).parent().next().children().addClass('checked');
+    }else{
+        $(e).parent().next().children().removeClass('checked');
+
+        
+    }
+
+}
+
